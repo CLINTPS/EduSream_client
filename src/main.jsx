9a0c65242f49e0080a6dacfa43cd9e84e55972c1
CLINTPS <clintps1010@gmail.com>
2024-10-22 11:39:21 +0530

    feat: google auth update

--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   
   <React.StrictMode>
     <Provider store={store}>
-    <GoogleOAuthProvider clientId='176344730742-d2215e6pfoilhu3phfrhfehfpci4sq3j.apps.googleusercontent.com'>
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
       <Toaster/>
         <App />
     </GoogleOAuthProvider>
